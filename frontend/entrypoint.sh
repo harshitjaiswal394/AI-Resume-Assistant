e0c86b86a1c23d931b8ccf28de7dc51f993777a4
Harshit Jaiswal <116247746+harshitjaiswal394@users.noreply.github.com>
2026-04-10 02:48:07 +0530

    updated entrypoint file

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
-# Replace placeholder with actual env var at runtime
+
+ENV="${ENVIRONMENT:-dev}"
+BACKEND_URL="http://${ENV}-backend-service:8000"
+
+echo "Using BACKEND_URL: $BACKEND_URL"
+
 sed -i "s|BACKEND_URL_PLACEHOLDER|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
+
 exec nginx -g "daemon off;"
