e7632d941afbe5b4e68be0161b282510d766bd73
Harshit Jaiswal <116247746+harshitjaiswal394@users.noreply.github.com>
2026-04-10 03:00:28 +0530

    updaed entrypoin & nginx file

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -2,9 +2,12 @@
 
 ENV="${ENVIRONMENT:-dev}"
 BACKEND_URL="http://${ENV}-backend-service:8000"
+RESOLVER=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
 
 echo "Using BACKEND_URL: $BACKEND_URL"
+echo "Using RESOLVER: $RESOLVER"
 
 sed -i "s|BACKEND_URL_PLACEHOLDER|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
+sed -i "s|RESOLVER_PLACEHOLDER|${RESOLVER}|g" /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
