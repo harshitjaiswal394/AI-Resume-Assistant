5d3ccceedd255d4da59e74c696d139fab63b31a2
harshit jaiswal <harshitjaiswal394@hotmail.com>
2026-04-10 13:22:26 +0530

    updated nginx file

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/sh
 
 ENV="${ENVIRONMENT:-dev}"
-BACKEND_URL="http://${ENV}-backend-service:8000"
-RESOLVER=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
+ECS_NAMESPACE="${ECS_NAMESPACE:-${ENV}-jobgpt}"
+BACKEND_SERVICE_NAME="${BACKEND_SERVICE_NAME:-${ENV}-backend-service}"
+BACKEND_URL="${BACKEND_URL:-http://${BACKEND_SERVICE_NAME}:8000}"
 
 echo "Using BACKEND_URL: $BACKEND_URL"
-echo "Using RESOLVER: $RESOLVER"
+echo "Using ECS_NAMESPACE: $ECS_NAMESPACE"
+echo "Using BACKEND_SERVICE_NAME: $BACKEND_SERVICE_NAME"
 
 sed -i "s|BACKEND_URL_PLACEHOLDER|${BACKEND_URL}|g" /etc/nginx/conf.d/default.conf
-sed -i "s|RESOLVER_PLACEHOLDER|${RESOLVER}|g" /etc/nginx/conf.d/default.conf
 
 exec nginx -g "daemon off;"
