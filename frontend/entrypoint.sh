e2cfdc4f549da3ddd880090f94f25e98fef43615
harshit jaiswal <harshitjaiswal394@hotmail.com>
2026-04-11 15:44:23 +0530

    updated nginx file

--- frontend/entrypoint.sh
+++ frontend/entrypoint.sh
@@ -3,7 +3,7 @@
 ENV="${ENVIRONMENT:-dev}"
 ECS_NAMESPACE="${ECS_NAMESPACE:-${ENV}-jobgpt}"
 BACKEND_SERVICE_NAME="${BACKEND_SERVICE_NAME:-${ENV}-backend-service}"
-BACKEND_URL="http://${BACKEND_SERVICE_NAME}.${ECS_NAMESPACE}:8000"
+BACKEND_URL="${BACKEND_URL:-http://${BACKEND_SERVICE_NAME}:8000}"
 RESOLVER=$(grep nameserver /etc/resolv.conf | awk '{print $2}' | head -1)
 
 echo "Using BACKEND_URL: $BACKEND_URL"
